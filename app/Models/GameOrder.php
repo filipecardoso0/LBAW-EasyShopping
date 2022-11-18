e1237e8f7f1decb693a0a8869d0502863675f0b3
Guilherme Pereira <gdrp77mdl@gmail.com>
2022-11-18 15:41:20 +0000

    Some model fixes

--- app/Models/GameOrder.php
+++ app/Models/GameOrder.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Item extends Model
+class GameOrder extends Model
 {
   // Don't add create and update timestamps in database.
   public $timestamps  = false;
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
