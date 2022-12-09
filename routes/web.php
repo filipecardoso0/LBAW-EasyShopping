7ca8852e4e3f295791cacf208b999dd92ae412bc
Guilherme Pereira <gdrp77mdl@gmail.com>
2022-12-09 10:39:41 +0000

    Atatic Pages Routes

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,6 @@ use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', 'OverviewController@index')->name('homepage');
 
-/*
-// Cards
-Route::get('cards', 'CardController@list');
-Route::get('cards/{id}', 'CardController@show');
-
-// API
-Route::put('api/cards', 'CardController@create');
-Route::delete('api/cards/{card_id}', 'CardController@delete');
-Route::put('api/cards/{card_id}/', 'ItemController@create');
-Route::post('api/item/{id}', 'ItemController@update');
-Route::delete('api/item/{id}', 'ItemController@delete');
-*/
-
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
@@ -42,13 +29,13 @@ Route::get('bestsellers', 'GameController@showBestSellers')->name('bestsellers')
 Route::get('comingsoon', 'GameController@showComingSoon')->name('comingsoon');
 Route::get('all', 'GameController@showAll')->name('viewallgames');
 
-//Product Information
+// Product Information
 Route::get('details/{game_id}', 'GameController@index')->name('game');
 
-//Categories
+// Categories
 Route::get('categories', 'CategoryController@index')->name('categories');
 
-//Shopping Cart
+// Shopping Cart
 Route::get('cart', 'ShoppingCartController@index')->name('shoppingcart'); //View Contents
 Route::post('addtocart', 'ShoppingCartController@store')->name('addtocart'); //Store Cart Contents (Authenticated Only)
 Route::delete('removefromcart', 'ShoppingCartController@destroy')->name('removefromcart'); //Removes Cart Item (Authenticated Only)
@@ -60,8 +47,17 @@ Route::get('guestCheckout', 'ShoppingCartController@guestItemstoCookie')->name('
 Route::get('checkout', 'OrderController@showPaymentGateway')->name('checkout');
 Route::post('finalize', 'OrderController@finalizePayment')->name('finalizePayment');
 
-//Dashboard
+// Dashboard
 Route::get('profile', 'UserController@index')->name('userprofile');
 
-//Search
-Route::get('/search', 'GameController@search')->name('search');
+// Search
+Route::get('search', 'GameController@search')->name('search');
+
+// About Us
+Route::get('about', 'StaticPagesController@showAbout')->name('about');
+
+// FAQ
+Route::get('faq', 'StaticPagesController@showFaqs')->name('faq');
+
+// Contact Us
+Route::get('contacts', 'StaticPagesController@showContacts')->name('contacts');
